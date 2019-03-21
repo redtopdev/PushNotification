1f8b258ba7ec55e54723b9bb3e44196dd8e24cbf
Atul Sagar <atul.sagar@ericsson.com>
2019-03-21 23:45:25 +0530

    modified

---
+++ PersistentSubscription/EventoMessageHadler.cs
@@ -0,0 +1,20 @@
+﻿using Engaze.Core.MessageBroker.Consumer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaListener
+{
+    public class EventoMessageHadler : IMessageHandler
+    {
+        public void OnError(string error)
+        {
+            Console.WriteLine(error);
+        }
+
+        public void OnMessageReceived(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- PersistentSubscription/Program.cs
+++ PersistentSubscription/Program.cs
@@ -1,51 +1,46 @@
-﻿
-using System;
-using System.Net;
-using System.Text;
-using EventStore.ClientAPI;
-using EventStore.ClientAPI.SystemData;
-
-namespace PersistentSubscription
+﻿namespace Engaze.PushNotificationKafkaListener
 {
-    /*
-    * This example sets up a volatile subscription to a test stream.
-    * 
-    * As written it will use the default ipaddress (loopback) and the default tcp port 1113 of the event
-    * store. In order to run the application bring up the event store in another window (you can use
-    * default arguments eg EventStore.ClusterNode.exe) then you can run this application with it. Once 
-    * this program is running you can run the WritingEvents sample to write some events to the stream
-    * and they will appear over the catch up subscription. You can also run many concurrent instances of this
-    * program and each will receive the events over the subscription.
-    * 
-    */
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using Engaze.Core.MessageBroker.Consumer;
+
     class Program
     {
-      
-        const int DEFAULTPORT = 1113;
-        const string STREAM = "$ce-Evento";
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
-            //uncommet to enable verbose logging in client.
-            var settings = ConnectionSettings.Create();//.EnableVerboseLogging().UseConsoleLogger();
-            using (var conn = EventStoreConnection.Create(settings, new IPEndPoint(IPAddress.Loopback, DEFAULTPORT)))
+           
+            new HostBuilder().ConfigureHostConfiguration(configHost =>
             {
-                conn.ConnectAsync().Wait();
-                
-                
-                conn.SubscribeToStreamAsync(STREAM, true, (_, x) =>
-                {
-                   
-                    var data = Encoding.ASCII.GetString(x.Event.Data);
-                    Console.WriteLine("Received: " + x.Event.EventStreamId + ":" + x.Event.EventNumber);
-                    Console.WriteLine(data);
-                }, null, new UserCredentials("admin", "changeit"));
+                configHost.AddCommandLine(args);
+                configHost.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            })
+            .ConfigureAppConfiguration((hostContext, configApp) =>
+            {
+                hostContext.HostingEnvironment.EnvironmentName = System.Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+            })
+             .ConfigureLogging((hostContext, configLogging) =>
+             {
+                 if (hostContext.HostingEnvironment.IsDevelopment())
+                 {
+                     configLogging.AddConsole();
+                     configLogging.AddDebug();
+                 }
+                 
+             }).ConfigureServices((hostContext, services) =>
+             {
+                 services.AddLogging();
+                 services.AddSingleton<IMessageHandler>();
+                 services.AddHostedService<EventoConsumer>();
 
-              
-            }
-            Console.WriteLine("waiting for events. press enter to exit");
+             })
+             .RunConsoleAsync();
+
             Console.ReadLine();
         }
     }
 }
+
+
